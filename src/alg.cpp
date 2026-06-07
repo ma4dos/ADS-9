0ea4b546d2bc2b1d018c7219995ddbeff6277e2b
ma4dos <dosuzhaya.mari@bk.ru>
2026-06-07 23:45:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 NNTU-CS
 #include "tree.h"
 
+#include <algorithm>
+#include <vector>
+
 PMTree::PMTree(const std::vector<char>& symbols) {
   std::vector<char> sorted = symbols;
   std::sort(sorted.begin(), sorted.end());
