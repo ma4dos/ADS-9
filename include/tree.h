a13161a546445924b46609ba23274222631d3ce4
ma4dos <dosuzhaya.mari@bk.ru>
2026-06-07 22:52:12 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
@@ -31,4 +32,4 @@ std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
 std::vector<char> getPerm1(const PMTree& tree, int num);
 std::vector<char> getPerm2(const PMTree& tree, int num);
 
-#endif
+#endif  // INCLUDE_TREE_H_
